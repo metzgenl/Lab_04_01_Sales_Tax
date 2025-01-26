83bde44d37280fce237cd91b1e0d981a08c550f0
Nate Metzger <metzgenl@mail.uc.edu>
2025-01-26 15:28:36 -0500

    Revisions to original code

--- src/SalesTax.java
+++ src/SalesTax.java
@@ -7,11 +7,8 @@ public class SalesTax {
     double salesTax;
 
     public void getSalesTax(){
-        //Create Scanner class
-        Scanner scn = new Scanner(System.in);
-        System.out.print("What is the cost of the item($): ");
-
-        purchasePrice = scn.nextDouble();
+        //Get input
+        purchasePrice = 100.00;
         salesTax = purchasePrice * 0.05;
         System.out.println("The item with a purchase price of " + purchasePrice + " has a sales tax of " + salesTax + ".");
 
